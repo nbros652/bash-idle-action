615427ca059adc1f1e598e3aa08e91af1ae0cef1
nbros652 <nbros652@users.noreply.github.com>
2020-08-12 14:39:27 -0400

    Update on-idle-service.sh
    added support for commands that access graphical desktop environments

--- on-idle-service.sh
+++ on-idle-service.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+display=:0                                # while locally logged into desktop environment, run `echo $DISPLAY` to find out what this value should be
+Xauthority=/run/user/1000/gdm/Xauthority  # while locally logged into desktop environment, run `echo $XAUTHORITY` to find out what this value should be
+
+export DISPLAY=$display           # this is needed for graphical desktop interaction
+export XAUTHORITY=$Xauthority     # this is needed for graphical desktop interaction
+
 idleTimeout=5400            # how many idle seconds to wait for shutdown
 idleCmd="shutdown -h 0"     # command to run when idleTimeout is reached
 checkInterval=10            # how many seconds between checks
