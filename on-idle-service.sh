df4b0cca4fc2150bd9ea58fc0fc7466a3c3a1a68
nbros652 <nbros652@users.noreply.github.com>
2020-08-13 00:10:51 -0400

    added comments; fixed run cmd then sleep

--- on-idle-service.sh
+++ on-idle-service.sh
@@ -8,33 +8,42 @@ checkInterval=10            # how many seconds between checks
 isIdle() {
     while read activeUser
     do
+        # get some session info
         user=$(awk '{print $1}' <<< "$activeUser");
         id=$(grep -P "^ccc:" /etc/passwd | cut -f3 -d:)
         display=$(awk '{print $2}' <<< "$activeUser");
+        
+        # export some environment vars specific to the active session
         export DISPLAY=$display
         export XAUTHORITY=/run/user/$id/gdm/Xauthority
+        
+        # calculate session idle time
         userIdle=$(su $user -c xprintidle)
         idleTime=$[ $userIdle / 1000 ]
+        
         if [ $idleTime -lt $idleTimeout ]; then
+            # return "false" if the idle timeout has not been reached
             return 1
         else 
+            # return true if the idle timeout has been reached
             return 0
         fi
     done <<< "$(who | grep -P "\s:\d")"
 # 	return $?
 }
 
+# start an infinite loop for checking idle timeout
 while :
 do
-    if ! isIdle; then
-        unset idle
-    fi
-    
+    # check if we are not idle or the idle command has already run
     if [ ! -z "$idle" ] || ! isIdle; then
+        isIdle || unset idle    # if we were idle, but no longer are, note the change
         sleep $checkInterval
         continue
     fi
+    
+    # We're idle, and the idleCmd has not been run; run it!
     idle=1
-    sleep $checkInterval
     eval "$idleCmd"
+    sleep $checkInterval
 done
